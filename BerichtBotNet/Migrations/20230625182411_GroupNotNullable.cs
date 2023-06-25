8a1a879890ea66ce0dfa2cd2a60a06838bee54fe
hendrik <dingerkus.hendrik@proton.me>
2023-06-25 21:51:49 +0200

    LazyLoading

---
+++ BerichtBotNet/Migrations/20230625182411_GroupNotNullable.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BerichtBotNet.Migrations
+{
+    /// <inheritdoc />
+    public partial class GroupNotNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Apprentices_Groups_GroupId",
+                table: "Apprentices");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GroupId",
+                table: "Apprentices",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Apprentices_Groups_GroupId",
+                table: "Apprentices",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Apprentices_Groups_GroupId",
+                table: "Apprentices");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GroupId",
+                table: "Apprentices",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Apprentices_Groups_GroupId",
+                table: "Apprentices",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "Id");
+        }
+    }
+}
