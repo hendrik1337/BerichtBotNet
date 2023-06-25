06bcf6ed941340b3baa58c24532ee9edaaa52267
Hendrik <dingerkus.hendrik@proton.me>
2023-06-25 15:14:21 +0200

    DiscordUserId is Unique

---
+++ BerichtBotNet/Migrations/20230625131307_DiscordUserIdIsUnique.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BerichtBotNet.Migrations
+{
+    /// <inheritdoc />
+    public partial class DiscordUserIdIsUnique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Apprentices_DiscordUserId",
+                table: "Apprentices",
+                column: "DiscordUserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Apprentices_DiscordUserId",
+                table: "Apprentices");
+        }
+    }
+}
