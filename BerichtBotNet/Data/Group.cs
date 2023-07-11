2919ee53df38cc9279d230d706074e413b0e2c80
Hendrik <dingerkus.hendrik@proton.me>
2023-07-11 11:10:22 +0200

    Bugfix, Timezones

--- BerichtBotNet/Data/Group.cs
+++ BerichtBotNet/Data/Group.cs
@@ -1,4 +1,7 @@
-﻿namespace BerichtBotNet.Data;
+﻿using System.Globalization;
+using BerichtBotNet.Helper;
+
+namespace BerichtBotNet.Data;
 
 /// <summary>
 /// Class which defines a Group for the BerichtBot
@@ -8,7 +11,14 @@ public class Group
     public int Id { get; set; }
     public string Name { get; set; } = null!;
     public string? DiscordGroupId { get; set; }
-    public DateTime StartOfApprenticeship { get; set; }
+
+    private DateTime _startOfApprenticeship;
+    public DateTime StartOfApprenticeship
+    {
+        get => TimeZoneInfo.ConvertTimeFromUtc(_startOfApprenticeship, TimeZoneInfo.Local);
+        set => _startOfApprenticeship = value;
+    }
+
     // Only Hour and Minute of ReminderTime is used. Date is ignored
     public DateTime ReminderTime { get; set; }
     public DayOfWeek ReminderWeekDay { get; set; }
