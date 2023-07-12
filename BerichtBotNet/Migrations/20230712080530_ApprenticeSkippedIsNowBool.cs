1f994045370255af6cbc0f89d867d72e851ca4b4
Hendrik <dingerkus.hendrik@proton.me>
2023-07-12 10:41:27 +0200

    apprenticeSkipBool

---
+++ BerichtBotNet/Migrations/20230712080530_ApprenticeSkippedIsNowBool.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BerichtBotNet.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApprenticeSkippedIsNowBool : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SkipCount",
+                table: "Apprentices");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Skipped",
+                table: "Apprentices",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Skipped",
+                table: "Apprentices");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SkipCount",
+                table: "Apprentices",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
